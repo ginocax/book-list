4e92b5b03e1ed157239e224de8ee4a33bbe4b55d
Giovanna Cinoca <gi.cinoca@hotmail.com>
2025-04-29 10:57:51 -0300

    feat: make progress bar dynamic in BookProgress component

--- src/components/BookProgress.vue
+++ src/components/BookProgress.vue
@@ -6,12 +6,17 @@ const props = defineProps(['books']);
 const booksRead = computed(() => {
   return props.books.filter(book => book.isRead).length;
 });
+
+const booksReadMessage = computed(() => {
+  return booksRead.value >= props.books.length ?
+  'Parabéns, você leu todos os livros!' : `${booksRead.value} de ${props.books.length} livros lidos`;
+});
 </script>
 
 <template>
   <div class="books-read">
     <label for="progress">Seu Progresso</label><br/>
-    <progress value="50" max="100"></progress>
-    <p>{{`${booksRead} de ${books.length} livros lidos`}}</p>
+    <progress :value="booksRead" :max="books.length"></progress>
+    <p>{{ booksReadMessage }}</p>
   </div>
 </template>
