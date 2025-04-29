dd58eb535d4207e48e1eae870d87e00a5e51fd4f
Giovanna Cinoca <gi.cinoca@hotmail.com>
2025-04-29 09:17:02 -0300

    feat: create Book.vue component

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,6 @@
 <script setup>
+import Books from './components/Books.vue';
+
 let books = [
   {
     id: 1,
@@ -52,24 +54,7 @@ let books = [
     </div>
  
     <div class="books-container">
-      <div class="books-list">
-        <div class="book">
-          <div class="book-cover">
-            <img src="https://printpress.cmsmasters.net/default/wp-content/uploads/sites/11/2019/05/printpress-product-6-540x861.jpg" />
-  
-            <button>
-              <i class="fa-solid fa-eye"></i>
-              <span>Ainda não li</span
-              >
-            </button>
-          </div>
-          <div class="book-details">
-            <p class="book-author">Daniel Trejo</p>
-            <h3 class="book-title">History of Europe</h3>
-            <p><i class="fa-solid fa-hashtag icon"></i> 0-395-07157-8</p>
-          </div>
-        </div>
-      </div>
+      <Books :books="books"/>
     </div>
   </div>
 </template>
---
+++ src/components/Books.vue
@@ -0,0 +1,24 @@
+<script setup>
+const props = defineProps(['books']);
+</script>
+
+<template>
+    <div class="books-list">
+        <div class="book">
+            <div class="book-cover">
+            <img src="https://m.media-amazon.com/images/I/811WfBrMtWL._AC_UF1000,1000_QL80_.jpg" />
+
+            <button>
+                <i class="fa-solid fa-eye"></i>
+                <span>Ainda não li</span
+                >
+            </button>
+            </div>
+            <div class="book-details">
+            <p class="book-author">Daniel Trejo</p>
+            <h3 class="book-title">History of Europe</h3>
+            <p><i class="fa-solid fa-hashtag icon"></i> 0-395-07157-8</p>
+            </div>
+        </div>
+    </div>
+</template>
